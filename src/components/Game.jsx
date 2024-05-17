f3bc9d4c56d44214aa480c7029980e2c1ac59444
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-17 16:32:12 -0700

    Adds in targetbox and styling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,7 +4,13 @@ import Gameboard from "./Gameboard"
 
 function Game (){
 
-    const [characters, setCharacters] = useState(["Washington Carver", "Curie", "Einstein", "Newton", "Ride"])
+    const [characters, setCharacters] = useState([
+        {name: "Washington Carver", x:0.166, y:0.374}, 
+        {name: "Curie", x:0.895, y:0.2925}, 
+        {name: "Einstein", x:0.7875, y:0.944}, 
+        {name: "Newton", x:0.145, y:0.824}, 
+        {name: "Ride", x:0.0325, y:0.1625}
+      ])
 
     return (
         <section className="game">
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -9,23 +9,21 @@ import {
 } from "../helpers/gameBoardHelper"
 
 function Gameboard({characters}){
+    const [guess, setGuess] = useState({x: null, y: null})
     const [targetPosition, setTargetPosition] = useState({ x:0, y: 0, show: false})
+    const [imgSize, setImgSize] = useState({w:0, h:0})
     const [dropdownHeight, setDropdownHeight] = useState(0)
+
     const dropdownMenu = useRef(null)
 
     const characterNames = characters
 
     const setAllStates = (e) => {
         const { guessX, guessY, targetX, targetY, imgWidth, imgHeight } = getClickPositions(e)
+        setGuess({x: guessX, y: guessY})
         setTargetPosition({x: targetX, y: targetY, show: true})
+        setImgSize({w: imgWidth, h: imgHeight})
         setDropdownHeight(0)
-        console.log(guessX)
-        console.log(guessY)
-        console.log(targetX)
-        console.log(targetY)
-        console.log(imgWidth)
-        console.log(imgHeight)
-
     }
 
     const handleGuess = (character) => {
@@ -33,7 +31,7 @@ function Gameboard({characters}){
     }
 
     const collapsible = () => {
-        setDropdownHeight(dropdownHeight === 0 ? "200px" : 0)
+        setDropdownHeight(dropdownHeight === 0 ? 200 : 0)
     }
 
     let style = {left:targetPosition.x + 'px', top:targetPosition.y + 'px', display: targetPosition.show ? 'flex' : 'none'}
@@ -41,14 +39,18 @@ function Gameboard({characters}){
     return (
         <div className="gameboard">
             <img src={gameImage} onClick={setAllStates}/>
-            <div style={style} className="dropdown">
+            <div style={style} className="guess">
+                <div className="guess__targetbox"></div>
+                <div className="dropdown">
                 <button className="btn btn__dropdown" onClick={collapsible}>Who is it? {'\u25bc'}</button>
                 <ul className="dropdown__menu" ref={dropdownMenu} style={{ maxHeight: dropdownHeight}}>
                     {characterNames.map((character, i) => {
-                        return <GuessButton key={i} onClick={() => handleGuess(character)}>{character}</GuessButton>
+                        return <GuessButton key={i} onClick={() => handleGuess(character)}>{character.name}</GuessButton>
                     })}
                 </ul>
             </div>
+            </div>
+
         </div>
     )
 }
