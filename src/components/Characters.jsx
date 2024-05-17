39aaf40dcb3b7882d90c932eec864867b15cd98d
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-17 14:36:01 -0700

    Adds in visible elements of play board and stylizes

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,12 +1,25 @@
+import PropTypes from 'prop-types'
 import characterImage from "../assets/characters.png"
 
-function Characters(){
+function Characters({characters}){
+
+    const characterNames = characters
+
     return (
         <div className="characters">
             <img src={characterImage}/> 
-            <div className="characters__foundList"></div>
+            <div className="characters__foundList">
+                {characterNames.map((name, i) => {
+                    let display = true
+                    return <span key={i} style={{visibility: display}} className="characters__found">Found</span>
+                })}
+            </div>
         </div>
     )
+}
+
+Characters.propTypes = {
+    characters: PropTypes.array,
 }
 
 export default Characters
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
 import Characters from "./Characters"
 import Gameboard from "./Gameboard"
 
 function Game (){
+
+    const [characters, setCharacters] = useState(["Washington Carver", "Curie", "Einstein", "Newton", "Ride"])
+
     return (
         <section className="game">
-            <Characters />
+            <Characters characters={characters} />
             <Gameboard />
         </section>
     )
