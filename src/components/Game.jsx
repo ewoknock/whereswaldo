b9e24821e6a0fb562b36e0b84394a10033065e33
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-21 13:30:07 -0700

    Moves character data from hardcoded frontend to retrieval from backend via API

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -9,22 +9,22 @@ const backEndUrl = "http://localhost:3000/"
 
 function Game (){
 
+    const [characters, setCharacters] = useState([])
+
     useEffect(() => {
         let url = backEndUrl + "api/v1/characters/"
-        console.log(url) 
         fetch(url)
             .then((response) => {
-                console.log(response.json())
+                if(response.ok){
+                    return response.json()
+                }
+                else{
+                    throw new Error("Error retrieving character data from API")
+                }
             })
+            .then((response) => setCharacters(response))
+            .catch()
     }, [])
-
-    const [characters, setCharacters] = useState([
-        {name: "Washington Carver", x:0.166, y:0.374}, 
-        {name: "Curie", x:0.895, y:0.2925}, 
-        {name: "Einstein", x:0.7875, y:0.944}, 
-        {name: "Newton", x:0.145, y:0.824}, 
-        {name: "Ride", x:0.0325, y:0.1625}
-      ])
 
     return (
         <section className="game">
